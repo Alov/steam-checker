7f759e504a302307e2cded2eaf898bde0f7e378f
Ilya <ilya@alov.me>
2022-07-28 20:46:56 +0300

    steam api calling

---
+++ src/main/java/me/alov/steamchecker/controller/CheckController.java
@@ -0,0 +1,34 @@
+package me.alov.steamchecker.controller;
+
+import me.alov.steamchecker.model.Info;
+import me.alov.steamchecker.model.SteamApiResponse;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/check")
+public class CheckController {
+
+    private static String CHECK_URL = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/";
+
+
+    @GetMapping
+    public ResponseEntity<Info> checkOnline(@RequestParam String steamId) {
+        RestTemplate sender = new RestTemplate();
+
+        String key = "54076FFC07F4E74FEB6215B533140F4E";
+
+        String fullUrl = CHECK_URL + "?key=" + key + "&" + "steamids=" + steamId;
+
+        SteamApiResponse steamApiResponse = sender.exchange(fullUrl, HttpMethod.GET, null, SteamApiResponse.class).getBody();
+
+        Info info = steamApiResponse.getResponse().getPlayers().get(0);
+
+        return ResponseEntity.ok(info);
+    }
+}
---
+++ src/main/java/me/alov/steamchecker/model/SteamApiResponse.java
@@ -0,0 +1,12 @@
+package me.alov.steamchecker.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SteamApiResponse {
+    private InfoList response;
+}
